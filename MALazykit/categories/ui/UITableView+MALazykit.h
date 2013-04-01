6406bbac0e31ba35c213c44a139ea764cc4a8804
Marlon Andrade <marlonmandrade@gmail.com>
2013-04-01 20:19:55 -0300

    adding uitableview lazyness

---
+++ MALazykit/categories/ui/UITableView+MALazykit.h
@@ -0,0 +1,15 @@
+//
+//  UITableView+MALazykit.h
+//  MALazykit
+//
+//  Created by Marlon Andrade on 01/04/13.
+//  Copyright (c) 2013 Marlon Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (MALazykit)
+
++ (instancetype)tableViewWithFrame:(CGRect)frame style:(UITableViewStyle)style;
+
+@end
---
+++ MALazykit/categories/ui/UITableView+MALazykit.m
@@ -0,0 +1,17 @@
+//
+//  UITableView+MALazykit.m
+//  MALazykit
+//
+//  Created by Marlon Andrade on 01/04/13.
+//  Copyright (c) 2013 Marlon Andrade. All rights reserved.
+//
+
+#import "UITableView+MALazykit.h"
+
+@implementation UITableView (MALazykit)
+
++ (instancetype)tableViewWithFrame:(CGRect)frame style:(UITableViewStyle)style {
+    return [[self alloc] initWithFrame:frame style:style];
+}
+
+@end
