d806e7fcad5eefa747e952a58945f99f46eddd26
Marlon Andrade <marlonmandrade@gmail.com>
2013-04-03 22:15:38 -0300

    adding UITableViewController lazyness

---
+++ MALazykit/categories/ui/UITableViewController+MALazykit.h
@@ -0,0 +1,15 @@
+//
+//  UITableViewController+MALazykit.h
+//  MALazykit
+//
+//  Created by Marlon Andrade on 03/04/13.
+//  Copyright (c) 2013 Marlon Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewController (MALazykit)
+
++ (instancetype)tableViewControllerWithStyle:(UITableViewStyle)style;
+
+@end
---
+++ MALazykit/categories/ui/UITableViewController+MALazykit.m
@@ -0,0 +1,17 @@
+//
+//  UITableViewController+MALazykit.m
+//  MALazykit
+//
+//  Created by Marlon Andrade on 03/04/13.
+//  Copyright (c) 2013 Marlon Andrade. All rights reserved.
+//
+
+#import "UITableViewController+MALazykit.h"
+
+@implementation UITableViewController (MALazykit)
+
++ (instancetype)tableViewControllerWithStyle:(UITableViewStyle)style {
+    return [[self alloc] initWithStyle:style];
+}
+
+@end
